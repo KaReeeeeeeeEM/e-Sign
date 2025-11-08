ee8cf6f6249978a312218c4c0d709cbaa318aec6
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2025-11-08 09:42:03 +0300

    Fix formatting and import statement in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { FaSignature } from "react-icons/fa"
-import DrawingBoard from "./components/Drawingboard"
+import { FaSignature } from "react-icons/fa";
+import DrawingBoard from "./components/DrawingBoard";
 
 function App() {
-
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen bg-neutral-50">
-      <span className="text-5xl mt-12 flex items-center gap-2"><span className="text-rose-500">e</span>-Sign <FaSignature className="text-rose-500" /></span>
+      <span className="text-5xl mt-12 flex items-center gap-2">
+        <span className="text-rose-500">e</span>-Sign{" "}
+        <FaSignature className="text-rose-500" />
+      </span>
       <DrawingBoard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
